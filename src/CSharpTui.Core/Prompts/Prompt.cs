ad3fe272bf1e585ee4c75a65595d463024b62fe4
albanog00 <albanog00@gmail.com>
2024-03-29 16:47:33 +0100

    fix
    added encapsulation for tui object in prompt class

--- src/CSharpTui.Core/Prompts/Prompt.cs
+++ src/CSharpTui.Core/Prompts/Prompt.cs
@@ -3,7 +3,7 @@ namespace CSharpTui.Core.Prompts;
 public abstract class Prompt<T> : IPrompt<T>
     where T : notnull
 {
-    public Tui Tui { get; set; }
+    protected Tui Tui { get; set; }
 
     public Prompt(Tui tui)
     {
