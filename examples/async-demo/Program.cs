1eaddbe42875a6ce6074d8cd7af3896bde5730dd
Giuseppe <albanog00@gmail.com>
2024-10-16 11:13:21 +0200

    async example

--- examples/async-demo/Program.cs
+++ examples/async-demo/Program.cs
@@ -1,18 +1,28 @@
 ﻿using CSharpTui.Prompts;
 
-var choices = new string[] { "A", "B", "C", "D", "E", "F" };
-
 Func<Func<int>> countFunction = () =>
 {
     int i = 0;
     return () => ++i;
 };
-
 Func<int> displayChoiceNumber = countFunction();
 
+var choices = new string[] { "A", "B", "C", "D", "E", "F" };
 var selectionPrompt = new SelectionPrompt<string>();
+for (int i = 0; i < choices.Length; ++i)
+{
+    var val = i;
+    Thread thread =
+        new(() =>
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(val + 1));
+            selectionPrompt.AddChoice(choices[val]);
+        });
+
+    thread.Start();
+}
+
 var value = selectionPrompt
-    .AddChoices(choices)
     .SetConverter(x => $"{displayChoiceNumber()}. {x.ToLower()}")
     .SetItemsOnScreen(5)
     .Show("Pick a choice");
